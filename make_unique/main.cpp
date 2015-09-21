99393941fd02d08ff417de6fe368faa0e95f2214
Jaroslaw Bober <jaroslaw.bober@gmail.com>
2015-09-21 22:39:17 +0200

    some unique_ptr examples

--- make_unique/main.cpp
+++ make_unique/main.cpp
@@ -1,10 +1,37 @@
 #include <iostream>
+#include <memory>
 
 using namespace std;
 
+class testing
+{
+public:
+    testing(){ cout << "ctor" << endl;}
+    ~testing(){ cout << "dtor"<< endl;}
+};
+class repository
+{
+private:
+    unique_ptr<testing> t_;
+public:
+
+    repository(unique_ptr<testing> t) : t_(move(t))
+    {
+
+    }
+};
+
+void sink(unique_ptr<testing> t)
+{
+
+}
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    repository r(unique_ptr<testing>(new testing));
+//    repository(unique_ptr<testing> (new testing)) t;
+    unique_ptr<testing> t(new testing);
+    sink(unique_ptr<testing>{new testing});
     return 0;
 }
 
