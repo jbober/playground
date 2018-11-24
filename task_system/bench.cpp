94b5bafc5c49b9be32868d48a5a627840cbd2a3e
jbober <jaroslaw.bober@gmail.com>
2018-11-24 22:05:59 +0100

    adding benchmark for fibonacci

--- task_system/bench.cpp
+++ task_system/bench.cpp
@@ -10,24 +10,14 @@
 
 using namespace boost::multiprecision;
 
-//static void std_async(benchmark::State& state) {
-//  for (auto _ : state)
-//  {
-//  std::string empty_string;
-//
-//  //std::future<cpp_int> x = std::async([]{ return fibonacci<cpp_int>(1'000'000); });
-//  //auto x = fibonacci<cpp_int>(1'000'000);
-//  //std::cout << x.get();
-//  }
-//}
-//BENCHMARK(std_async);
-//
-static void BM_StringCreation(benchmark::State& state) {
+static void std_async(benchmark::State& state) {
   for (auto _ : state)
-    std::string empty_string;
+  {
+
+  std::future<cpp_int> x = std::async([]{ return fibonacci<cpp_int>(1'000'000); });
+  }
 }
-// Register the function as a benchmark
-BENCHMARK(BM_StringCreation);
+BENCHMARK(std_async);
 
 BENCHMARK_MAIN();
 
