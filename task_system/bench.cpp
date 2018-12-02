aa840438cb10a6891168b86b229fd5520baeb0d9
jbober <jaroslaw.bober@gmail.com>
2018-12-02 20:38:43 +0000

    adding benchmark for stlab

--- task_system/bench.cpp
+++ task_system/bench.cpp
@@ -9,6 +9,8 @@
 #include <string>
 
 #include <stlab/concurrency/channel.hpp>
+#include <stlab/concurrency/future.hpp>
+#include <stlab/concurrency/default_executor.hpp>
 
 using namespace boost::multiprecision;
 using namespace std;
@@ -18,9 +20,20 @@ static void std_async(benchmark::State& state) {
   {
 
   future<cpp_int> x = std::async([]{ return fibonacci<cpp_int>(1'000'000); });
+  //future<cpp_int> y = std::async([]{ return fibonacci<cpp_int>(1'000'000); });
   }
 }
 BENCHMARK(std_async);
+
+static void stlab_async(benchmark::State& state) {
+  for (auto _ : state)
+  {
+
+  stlab::future<cpp_int> x = stlab::async(stlab::default_executor, []{ return fibonacci<cpp_int>(1'000'000); });
+  //stlab::future<void> y = x.then([](stlab::future<cpp_int> x){ std::cout << x.get() << std::endl; });
+  }
+}
+BENCHMARK(stlab_async);
 
 BENCHMARK_MAIN();
 
